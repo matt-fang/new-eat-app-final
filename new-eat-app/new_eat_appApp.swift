4ad61dadf70f1f2ff55575b7d7de2dee50200370
matt-fang <matthewfang.calif@gmail.com>
2025-02-07 11:11:41 -0800

    Initial Commit

---
+++ new-eat-app/new_eat_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  new_eat_appApp.swift
+//  new-eat-app
+//
+//  Created by Matthew Fang on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct new_eat_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ new-eat-appTests/new_eat_appTests.swift
@@ -0,0 +1,16 @@
+//
+//  new_eat_appTests.swift
+//  new-eat-appTests
+//
+//  Created by Matthew Fang on 2/7/25.
+//
+
+import Testing
+
+struct new_eat_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
